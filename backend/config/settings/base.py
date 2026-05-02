f9a38ee6bbc672c2bae9608192347387cae49a78
Joygloria <joygloriam335@gmail.com>
2026-05-02 14:07:25 +0300

    created translation middlware to intercept API responses and translate them

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -37,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'apps.ai_layer.translation.middleware.TranslationMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
